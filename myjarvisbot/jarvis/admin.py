c6bdb416f496c0e6efc6e596174da5243f126c1b
Eliézer Bourchardt <eliezerfb@gmail.com>
2018-07-08 16:40:28 -0300

    register admin

--- myjarvisbot/jarvis/admin.py
+++ myjarvisbot/jarvis/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from myjarvisbot.jarvis.models import Lista, ItensLista
 
 # Register your models here.
+admin.site.register(Lista)
+admin.site.register(ItensLista)
