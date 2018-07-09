cf2a7c50046c02f53424161816361dbbe220ca9d
eliezerfb <eliezerfb@gmail.com>
2018-07-09 17:26:54 -0300

    adiciona print para verificar log

--- myjarvisbot/jarvis/views.py
+++ myjarvisbot/jarvis/views.py
@@ -21,6 +21,7 @@ def _insert_item_lista(command):
     produto = command.split(',')
     quantidade = '' if len(produto) < 2 else produto[1].strip()
     obj = ItensLista(produto=produto[0], quantidade=quantidade)
+    print(produto[0])
     obj.save()
 
 
