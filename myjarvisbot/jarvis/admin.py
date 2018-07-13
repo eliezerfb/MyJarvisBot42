54c3e59181b2c38641cdffd54f93eeb4ecf37757
eliezerfb <eliezerfb@gmail.com>
2018-07-13 16:45:19 -0300

    adiciona modelo userstelegram no admin

--- myjarvisbot/jarvis/admin.py
+++ myjarvisbot/jarvis/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from myjarvisbot.jarvis.models import ItensLista
+from myjarvisbot.jarvis.models import ItensLista, UsersTelegram
 
 # Register your models here.
 admin.site.register(ItensLista)
+admin.site.register(UsersTelegram)
