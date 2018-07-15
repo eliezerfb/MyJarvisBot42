2077688aff21768fb0e9eea3bac1958c0aaf5b52
Eliézer Bourchardt <eliezerfb@gmail.com>
2018-07-15 20:16:24 -0300

    adiciona secao

--- myjarvisbot/jarvis/models.py
+++ myjarvisbot/jarvis/models.py
@@ -3,13 +3,27 @@ from django.db import models
 
 
 class ItensLista(models.Model):
+    SECOES = (
+        (10, 'Cereais'),
+        (20, 'Mercearia'),
+        (30, 'Frios'),
+        (40, 'Hortifruti'),
+        (50, 'Carnes'),
+        (60, 'Higiene'),
+        (70, 'Limpeza'),
+        (80, 'Bebidas'),
+        (90, 'Outros')
+    )
     semana = models.PositiveSmallIntegerField(
-        default=datetime.today().isocalendar()[1]
+        default=int(datetime.today().strftime('%U'))
     )
     ano = models.PositiveSmallIntegerField(default=datetime.today().year)
     produto = models.CharField(max_length=50)
     quantidade = models.CharField(max_length=10, default='', blank=True)
     categoria = models.CharField(max_length=10, default='', blank=True)
+    secao = models.PositiveSmallIntegerField(
+        'seção', choices=SECOES, blank=True, default=90
+    )
 
     class Meta:
         verbose_name_plural = 'itens da lista'
