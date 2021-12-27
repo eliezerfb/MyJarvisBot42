5fe40ec3a0ad32a27fadd1fae9d735b63b2b6687
eliezerfb <eliezerfb@gmail.com>
2021-12-27 11:40:35 -0300

    Corrige monitor de NF-e

--- myjarvisbot/jarvis/bot_news/bot.py
+++ myjarvisbot/jarvis/bot_news/bot.py
@@ -17,6 +17,7 @@ from http.cookiejar import CookieJar
 
 
 
+
 def exists_reported(title):
     from myjarvisbot.jarvis.models import NewsReported
     news = NewsReported.objects.filter(title=title[:100])
@@ -104,10 +105,10 @@ for site in sites_monitor:
 
 ##### Monitor SEFAZ ######
 hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
-# {'site': "https://www.nfe.fazenda.gov.br/portal/informe.aspx?ehCTG=false", 'doc':'NF-e'},
 sites_monitor = [
-                 {'site': "https://www.cte.fazenda.gov.br/portal/listaConteudo.aspx?tipoConteudo=Y0nErnoZpsg=", 'doc':'CT-e'}]
-print('NF-e/CT-e')
+                 {'site': "https://www.cte.fazenda.gov.br/portal/listaConteudo.aspx?tipoConteudo=Y0nErnoZpsg=", 'doc':'CT-e'}
+                ]
+print('NF-e')
 for site in sites_monitor:
     req = Request(site['site'], headers=hdr)
     page = urlopen(req)
@@ -131,7 +132,34 @@ for site in sites_monitor:
 
             r.post(url_hornC4, json=data, headers=headers)
             time.sleep(5.0)
+            
 
+sites_monitor = [
+                 {'site': "https://www.nfe.fazenda.gov.br/portal/informe.aspx?ehCTG=false", 'doc':'NF-e'}
+                ]
+print('NF-e')
+for site in sites_monitor:
+    cj = CookieJar()
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    req = Request(site['site'], None, {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3','Accept-Encoding': 'gzip, deflate, sdch','Accept-Language': 'en-US,en;q=0.8','Connection': 'keep-alive'})
+    page = opener.open(req)
+    soup = BeautifulSoup(page.read(), features="html.parser")
+
+    noticias_relacao = soup.find_all("div", attrs={"class": "divInforme"})
+    for noticia in noticias_relacao:
+        all_p = noticia.find_all("p")
+        for p in all_p:
+            conteudo = p.text.strip()
+            url = site['site']
+            data = {"text": f'{titulo}\n{conteudo}\n{url}\n'}
+    
+            if exists_reported(titulo):
+                continue
+
+            add_title(titulo)
+
+            r.post(url_hornC4, json=data, headers=headers)
+            time.sleep(5.0)
             
 
 
@@ -177,7 +205,7 @@ for site in sites_monitor:
 
 
 ######  RADIO RURAL   #######
-
+print('Rádio Rural')
 site = "http://www.radiorural.com.br/noticias/"
 hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
 req = Request(site, headers=hdr)
@@ -205,7 +233,7 @@ for noticia in noticias_relacao:
 
 
 ######  ATUAL FM   #######
-
+print('Rádio Atual')
 site= "https://www.atualfm.com.br/site/category/ultimas-noticias/"
 hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
 req = Request(site,headers=hdr)
