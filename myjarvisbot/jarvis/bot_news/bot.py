3ff5cb03df39509ffb22f9aee7b29a85fbd66378
Eliézer Bourchardt <eliezerfb@gmail.com>
2022-04-19 16:02:28 -0300

    adiciona titulo na base

--- myjarvisbot/jarvis/bot_news/bot.py
+++ myjarvisbot/jarvis/bot_news/bot.py
@@ -259,6 +259,8 @@ try:
         if exists_reported(titulo):
           continue
 
+        add_title(titulo)
+
         data = {"text": f'{titulo}\n{url}\n'}
         print(data)
         # r.post(url_horn, json=data, headers=headers)
